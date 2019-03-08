050fbe45eb232a2ada835f3d06319e5b79a62aea
Ma <yma2@caltech.edu>
2019-03-07 21:45:32 -0800

    Finish loadShake

--- DataProcessing.py
+++ DataProcessing.py
@@ -1,27 +1,37 @@
 import numpy as ny
 import re
 
-sonnetNumLine = re.compile(r'\s+\d+\n')
+sonnetNumLineRe = re.compile(r'\s+\d+\n')
+wordPuncRe = re.compile(r"[\w']+|[.,!?;]")
+wordOnlyRe = re.compile(r"[\w']+")
 
-def loadShake():
+
+# Function to load data of Shakespear to a list of sonnets.
+# Each sonnet is further seperated into lines
+# Each line is further seperated into words and punctruations unless specified (then punctruations will be ignored)
+# Word like "Feed'st" and "youth's" will be treated as one single word
+def loadShake(sepPuncs=True):
     sonnets = []
     with open('./data/shakespeare.txt', 'r') as f:
         newSonnet = []
         for line in iter(lambda: f.readline(), ''):
-            if sonnetNumLine.match(line):
+            if sonnetNumLineRe.match(line):
                 sonnets.append(newSonnet)
                 newSonnet = []
             else:
                 if not line == '\n':
-                    newSonnet.append(line)
+                    if sepPuncs:
+                        splitLine = wordPuncRe.findall(line)
+                    else:
+                        splitLine = wordOnlyRe.findall(line)
+                    newSonnet.append(splitLine)
 
         sonnets.append(newSonnet)
-
         sonnets = sonnets[1:]
     
     return sonnets
 
 
 if __name__ == '__main__':
-    sonnets = loadShake()
+    sonnets = loadShake(False)
     pass
