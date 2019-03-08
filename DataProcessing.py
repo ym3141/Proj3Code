e9843b6daffee9e4b1595544cd3006c913926dac
Ma <yma2@caltech.edu>
2019-03-07 22:22:19 -0800

    Added load syllable

--- DataProcessing.py
+++ DataProcessing.py
@@ -2,15 +2,18 @@ import numpy as ny
 import re
 
 sonnetNumLineRe = re.compile(r'\s+\d+\n')
-wordPuncRe = re.compile(r"[\w']+|[.,!?;]")
-wordOnlyRe = re.compile(r"[\w']+")
+wordPuncRe = re.compile(r"[\w'-]+|[.,!?;]")
+wordOnlyRe = re.compile(r"[\w'-]+")
 
 
 # Function to load data of Shakespear to a list of sonnets.
 # Each sonnet is further seperated into lines
 # Each line is further seperated into words and punctruations unless specified (then punctruations will be ignored)
 # Word like "Feed'st" and "youth's" will be treated as one single word
-def loadShake(sepPuncs=True):
+# Hyphened words are also treated as one word
+# Single quotes not in the middle of the word will be ignored
+
+def loadShake(sepPuncs=True, ignoreSingleQuotes=False):
     sonnets = []
     with open('./data/shakespeare.txt', 'r') as f:
         newSonnet = []
@@ -24,6 +27,17 @@ def loadShake(sepPuncs=True):
                         splitLine = wordPuncRe.findall(line)
                     else:
                         splitLine = wordOnlyRe.findall(line)
+
+                    if ignoreSingleQuotes:
+                        for idx, word in enumerate(splitLine):
+                            newWord = word
+                            if word.endswith('\''):
+                                newWord = newWord[0:-1]
+                                splitLine[idx] = newWord
+                            elif word.startswith('\''):
+                                newWord = newWord[1:]
+                                splitLine[idx] = newWord
+                    splitLine = [x for x in splitLine if x]
                     newSonnet.append(splitLine)
 
         sonnets.append(newSonnet)
@@ -32,6 +46,26 @@ def loadShake(sepPuncs=True):
     return sonnets
 
 
+# function to return a dict of the syllable dict
+# key will be word, and value will be a tuple that contains several possible number of syllables
+# note values in tuples are all str, since we have to deal with things like "E1"
+# example: 
+# {"'gainst": ('1',), "'greeing": ('E1', '2'), "'scaped": ('1',) ... } 
+def loadSyllable():
+    syllDict = dict()
+    with open('./data/Syllable_dictionary.txt') as f:
+        for line in iter(lambda: f.readline(), ''):
+            splitLine = line.split()
+            syllDict[splitLine[0]] = tuple(splitLine[1:])
+    return syllDict
+
+
 if __name__ == '__main__':
-    sonnets = loadShake(False)
+    # check if the every word loaded from Shakespear can be found in the syllable dict 
+    syllDict = loadSyllable()
+    for sonnet in loadShake(False):
+        for line in sonnet:
+            for word in line:
+                if not word.lower() in syllDict:
+                    print(word) 
     pass
