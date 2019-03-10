e9576de8acbcefd8648408f88eb9db6154c36bc2
Christina Su <csu@caltech.edu>
2019-03-10 15:25:32 -0700

    Add function to load data for character-based models.

--- DataProcessing.py
+++ DataProcessing.py
@@ -1,13 +1,14 @@
 import re
+import string
 
 sonnetNumLineRe = re.compile(r'\s+\d+\n')
 wordPuncRe = re.compile(r"[\w'-]+|[.,!?;]")
 wordOnlyRe = re.compile(r"[\w'-]+")
 
-# function to return a dict of the syllable dict
-# key will be word, and value will be a tuple that contains several possible number of syllables
-# note values in tuples are all str, since we have to deal with things like "E1"
-# example: 
+# Function to return a dict of the syllable dictionary
+# Key will be word, and value will be a tuple that contains several possible number of syllables
+# Note values in tuples are all str, since we have to deal with things like "E1"
+# Example: 
 # {"'gainst": ('1',), "'greeing": ('E1', '2'), "'scaped": ('1',) ... } 
 def loadSyllable():
     syllDict = dict()
@@ -18,15 +19,15 @@ def loadSyllable():
     return syllDict
 
 
-# Function to load sonnets and syllable dictionary of Shakespear.
+# Function to load sonnets and syllable dictionary of Shakespeare
 # For sonnets 
-    # Each sonnet is further seperated into lines
-    # Each line is further seperated into words and punctruations unless specified (then punctruations will be ignored)
-    # Turn on "mathchSyllableDic" to make all word has a syllable dictionary entry (some messy work)
+    # Each sonnet is further separated into lines
+    # Each line is further separated into words and punctuation unless specified (then punctuation will be ignored)
+    # Turn on "matchSyllableDic" to make all words have a syllable dictionary entry (some messy work)
     # toLower will turn everything to lower case 
 # For syllable dict:
-    # key will be word, and value will be a tuple that contains several possible number of syllables
-    # note values in tuples are all str, since we have to deal with things like "E1" 
+    # Key will be word, and value will be a tuple that contains several possible number of syllables
+    # Note values in tuples are all str, since we have to deal with things like "E1" 
 def loadShake(sepPuncs=True, matchSyllableDic=True, toLower=True):
     syllableDic = loadSyllable()
     sonnets = []
@@ -70,9 +71,76 @@ def loadShake(sepPuncs=True, matchSyllableDic=True, toLower=True):
     return sonnets, syllableDic
 
 
-# Function to load and encode sonnets and syllable dictionary of Shakespear.
-    # Also return some useful dictionaries: the dict for number to word/punctruation/newline & the dict for punctruation to number
-    # refers to loadShake comments for more detailed explanation.
+# Function to load Shakespearean sonnets and syllable dictionary for
+# character-based model
+# sonnets: list in which each element represents a sonnet
+    # stripPunc=True: remove punctuation (except in words) and newline tokens
+    # toLower=True: convert to lower case
+    # matchSyllableDic=True: ensure that all words are in syllable dictionary
+    # (does not apply if stripPunc=False)
+# syllableDic: dict representing syllable dictionary
+    # See loadSyllable() for full documentation
+def loadShake_char(stripPunc=True, toLower=True, matchSyllableDic=True):
+    sonnets = []
+    syllableDic = loadSyllable()
+    
+    # Read in data
+    with open('./data/shakespeare.txt', 'r') as f:
+        sonnet = ''
+        for line in iter(lambda: f.readline(), ''):
+            # Check for beginning of sonnet
+            if sonnetNumLineRe.match(line):
+                sonnets.append(sonnet)
+                sonnet = ''
+            
+            # Process nonempty lines
+            elif line != '\n':
+                # Append full line if punctuation/spacing are not stripped
+                if not stripPunc:
+                    if toLower:
+                        sonnet += line.lower()
+                    else:
+                        sonnet += line
+                    continue
+                    
+                # Split line into words
+                splitLine = wordPuncRe.findall(line)
+                
+                # Check that all words are in syllable dictionary if specified
+                if matchSyllableDic:
+                    for idx, word in enumerate(splitLine):
+                        # Strip punctuation
+                        if word in string.punctuation:
+                            splitLine[idx] = ''
+                        elif not word.lower() in syllableDic:
+                            if word[:-1].lower() in syllableDic:
+                                splitLine[idx] = word[:-1]
+                            elif word[1:].lower() in syllableDic:
+                                splitLine[idx] = word[1:]
+                            elif word[1:-1].lower() in syllableDic:
+                                splitLine[idx] = word[1:-1]
+                            else:
+                                print('Word', word, 'rejected.')
+                                splitLine[idx] = ''
+                
+                # Add line stripped of punctuation and newline tokens
+                if toLower:
+                    splitLine = [word.lower() for word in splitLine if word]
+                else:
+                    splitLine = [word for word in splitLine if word]
+                if sonnet != '':
+                    sonnet += ' '
+                sonnet += ' '.join(splitLine)
+                
+        sonnets.append(sonnet)
+        sonnets = sonnets[1:]
+        
+    return sonnets, syllableDic
+
+
+# Function to load and encode sonnets and syllable dictionary of Shakespeare
+    # Also return some useful dictionaries: the dict for number to word/punctuation/newline & the dict for punctuation to number
+    # Refer to loadShake comments for more detailed explanation
 def encodedShake():
     sonnets, syllaDict = loadShake()
     wordSetList = []
@@ -110,7 +178,7 @@ def code2sonnet(codes, code2word):
 
 
 if __name__ == '__main__':
-    # check if the every word loaded from Shakespear can be found in the syllable dict 
+    # check if every word loaded from Shakespeare can be found in the syllable dict
     sonnets, syllDict = loadShake(sepPuncs=False)
     for sonnet in sonnets:
         for line in sonnet:
