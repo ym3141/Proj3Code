3704ca1b15f88303ab368f333a23b10dd70a2611
ym3141 <yma2@caltech.edu>
2019-03-13 20:22:38 -0700

    Code for visulization of the HMM

--- HMMsonnet_YM.py
+++ HMMsonnet_YM.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from HMM_fast import HiddenMarkovModel, unsupervised_HMM
 from DataProcessing import encodedShake, Convert2SonnetNaive
 
@@ -5,11 +7,14 @@ from DataProcessing import encodedShake, Convert2SonnetNaive
 if __name__ == '__main__':
     encodedSonnets, encodedSyllaDict, code2word, punc2code = encodedShake()
 
-    HMmodel = unsupervised_HMM(encodedSonnets, 40, 50)
+    HMmodel = unsupervised_HMM(encodedSonnets, 40, 200)
 
     print(Convert2SonnetNaive(encodedSonnets[0], code2word)[0])
     print(Convert2SonnetNaive(encodedSonnets[0], code2word)[1])
 
-    sonnet = HMmodel.generate_emission(200)[0]
+    sonnet = HMmodel.generate_emission(300)[0]
     
     print(Convert2SonnetNaive(sonnet, code2word)[1])
+
+    np.save('./TrainingTemp/HMM.A.npy', HMmodel.A)
+    np.save('./TrainingTemp/HMM.O.npy', HMmodel.O)
